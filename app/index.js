7ff6a7f6ae5185064ae924bb503fc3442a99a0bb
jovanka <jovanka.placeres@gmail.com>
2018-09-22 02:51:38 +0000

    streamers in WatchList component have onClick functionality; added constructor/state within App component; handle function for WatchList streamers, taking arg of streamers; prop added to WatchList component inserted into render method of App component; handle function bind within constructor; onWatch, prop added to WatchList comp within App component render method used with onClick of WatchList component; clicks registering basic console.log atm

--- app/index.js
+++ app/index.js
@@ -2,20 +2,37 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('./index.css')
 
-function WatchList() {
+function WatchList(props) {
 	const streamers = ['relaxbeats', 'freecodecamp', 'funfunfunction', 'anathug_', '24_7_chill_piano'];
 	return (
 		streamers.map((streamer) =>
-			<li key={streamer}>{streamer}</li>
+			<li key={streamer} onClick={() => props.onWatch(streamer)}>{streamer}</li>
 		)
 	)
 }
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			streamers: [],
+		}
+
+		this.handleWatchList = this.handleWatchList.bind(this);
+	}
+
+	handleWatchList(streamer) {
+		console.log(streamer)
+		this.setState({
+			streamers: streamer,
+		})
+	}
+
 	render() {
 		return (
 			<div>
-				<WatchList />
+				<WatchList onWatch={this.handleWatchList}/>
 			</div>
 		)
 	}
