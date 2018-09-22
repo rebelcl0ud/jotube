1cfad8a08345b32139936b2cbe4c40d3861a6ef4
jovanka <jovanka.placeres@gmail.com>
2018-09-22 01:14:12 +0000

    created WatchList component; streamers mapped over to create list; added component to App component in order to render onto browser; no functionality yet, basic list

--- app/index.js
+++ app/index.js
@@ -2,11 +2,20 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('./index.css')
 
+function WatchList() {
+	const streamers = ['relaxbeats', 'freecodecamp', 'funfunfunction', 'anathug_', '24_7_chill_piano'];
+	return (
+		streamers.map((streamer) =>
+			<li key={streamer}>{streamer}</li>
+		)
+	)
+}
+
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>hebwooooo</h1>
+				<WatchList />
 			</div>
 		)
 	}
