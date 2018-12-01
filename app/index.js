9d84bc6c4d751041a68f2486dbfd201225e83103
jovanka <jovanka.placeres@gmail.com>
2018-12-01 05:15:44 +0000

    reworked the API fetches, added Navbar, some light styling in StreamerDeets, added another ReactDOM.render for the Navbar component, commented out prev work regarding WatchList-- presently API fetches info/ displaying default state/streamer

--- app/index.js
+++ app/index.js
@@ -2,31 +2,49 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('./index.css')
 
-function WatchList(props) {
-	const streamers = ['relaxbeats', 'freecodecamp', 'funfunfunction', 'anathug_', '24_7_chill_piano'];
+/**
+
+**/
+
+// function WatchList(props) {
+// 	const streamers = ['relaxbeats', 'freecodecamp', 'funfunfunction', 'anathug_', '24_7_chill_piano'];
+	
+// 	return (
+// 		streamers.map((streamer) => 
+// 			<li key={streamer} onClick={() => props.onWatch(streamer)}>{streamer}</li>	
+// 		)
+// 	)
+// }
+
+const Navbar = () => {
 	return (
-		streamers.map((streamer) =>
-			<li key={streamer} onClick={() => props.onWatch(streamer)}>{streamer}</li>
-		)
+		<nav class="navbar navbar-dark mb-5">
+			<span className='navbar-brand mb-0 h1'>joTube</span>
+		</nav>
 	)
 }
 
 function StreamerDeets(props) {
 	// display stream deets
 	return (
-		<div>
-			<ul>
-				{props.streams.map(({display_name, login, description, view_count, profile_image_url}) => (
-					<ul key={display_name}>
-						<img src={profile_image_url}/>
-						<li><a href=''>{display_name}</a></li>
-						<li>{description}</li>
-						<li>{view_count}</li>
-					</ul>
-				))}
-			</ul>
+		<div className='row'>
+			<div className='col-md-8'>
+				<div className='card-mb-4 shadow-sm'>
+					<div className='card-body'>
+						<ul>
+							{props.streams.map(({display_name, login, description, view_count, profile_image_url}) => (
+								<ul key={display_name}>
+									<img src={profile_image_url}/>
+									<li><a href=''>{display_name}</a></li>
+									<li>{description}</li>
+									<li>{view_count}</li>
+								</ul>
+							))}
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
-
 	)
 }
 
@@ -37,51 +55,79 @@ class App extends React.Component {
 		this.state = {
 			streamers: ['relaxbeats'],
 			streams:[],
+			onlineID: [],
 		}
 
-		this.handleWatchList = this.handleWatchList.bind(this);
+		// this.handleWatchList = this.handleWatchList.bind(this);
 		this.fetchStreamerData = this.fetchStreamerData.bind(this);
 	}
 
+	componentDidCatch(error, info) {
+		console.error(error);
+		console.warn(info);
+	}
+
 	componentDidUpdate(prevProp, prevState) {
+		console.log('componentDidUpdate called')
 		if(this.state.streamers !== prevState.streamers) {
 			this.fetchStreamerData(this.state.streamers)
 		}
 	}
 
 	componentDidMount() {
+		console.log('componentDidMount called')
 		this.fetchStreamerData(this.state.streamers);
 	}
 
 	fetchStreamerData(streamer) {
-		// calls API
+		// calls API [user]
 		const clientID = 'aoduqmn77opdqjxy9aqirf976btdpk'; // can be public
-	
 		fetch(`https://api.twitch.tv/helix/users?login=${streamer}`, { headers: {'Client-ID': clientID}})
-			.then((data) => data.json())
+			.then((res) => res.json())
 			.then((streams) => {
-				// console.log(streams)
+				console.log(streams.data)
 					this.setState({
 						streams: streams.data,
 					})
+
+					return fetch(`https://api.twitch.tv/helix/streams?user_login=${streamer}`, { headers: {'Client-ID': clientID}})
 			})
+			.then((res) => res.json())
+			.then((streaming) => {
+				console.log(streaming.data)
+			// 	streaming.data.map(({type, user_id}) => {
+			// 		if(type == 'live') {
+			// 			this.setState({
+			// 				onlineID: user_id
+			// 			})
+			// 		}
+			// 	})
+			})
+			.catch((err) => {
+				console.error(err);
+			})
+			
 	}
 
-	handleWatchList(streamer) {
-		// console.log(streamer)
-		this.setState({
-			streamers: streamer,
-		})
-	}
+
+	// handleWatchList(streamer) {
+	// 	// console.log(streamer)
+	// 	this.setState({
+	// 		streamers: streamer,
+	// 	})
+	// }
+
+	// removed below: <WatchList onWatch={this.handleWatchList}/>
 
 	render() {
 		return (
-			<div>
-				<WatchList onWatch={this.handleWatchList}/>
+			
+			<div className='container'>
 				<StreamerDeets streams={this.state.streams}/>
 			</div>
 		)
 	}
 }
 
+ReactDOM.render( <Navbar />, document.getElementById('nav') )
 ReactDOM.render( <App />, document.getElementById('app') )
